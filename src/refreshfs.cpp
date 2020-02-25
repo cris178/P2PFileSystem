09c98779ba24dd67be2170eb9b948370dd66fffd
danny <danialesaid@gmail.com>
2020-02-24 19:46:20 -0800

    Prortype for connecting make a file to opendht work

--- src/refreshfs.cpp
+++ src/refreshfs.cpp
@@ -15,6 +15,16 @@
 #include <stdlib.h>
 #include <errno.h>
 
+
+
+#include <iostream>
+#include <vector>
+#include <opendht.h>
+
+
+using std::cout;
+using std::endl;
+
 #define PATH_MAX 4096
 
 struct refreshfs_dirp
@@ -30,6 +40,14 @@ static struct mountpoint
 	struct refreshfs_dirp *dir;
 	char *path;
 } mountpoint;
+
+
+
+
+ dht::DhtRunner node;
+
+
+
 
 int order = 0;
 // ... //
@@ -597,6 +615,12 @@ static int do_write(const char *path, const char *buffer, size_t size, off_t off
 
 	retstat = pwrite(fi->fh, buffer, size, offset);
 
+
+
+	cout << "--------------------------------------------in do_write Path: " << path << " buffer: " << buffer << endl; 
+	node.put(path, buffer);
+
+
 	if(retstat < 0)
 	{
 		perror("error in do_write ");
@@ -767,10 +791,79 @@ int main(int argc, char *argv[])
 	// closedir(mountpoint.dir->dp);
 	// free(mountpoint.path);
 
+
+
+
+std::cout << "starting DHT stuff" << std::endl; 
+
+
+    // Launch a dht node on a new thread, using a
+    // generated RSA key pair, and listen on port 4222.
+    node.run(4222, dht::crypto::generateIdentity(), true);
+
+    // Join the network through any running node,
+    // here using a known bootstrap node.
+    node.bootstrap("10.0.2.4", "53826");
+
+    // put some data on the dht
+    // std::vector<uint8_t> some_data(5, 10);
+    // // some_data.push_back(5);
+    // node.put("K7", some_data);
+
+    // put some data on the dht, signed with our generated private key
+    // node.putSigned("K6", "V6");
+
+    // // put some data on the dht
+    // std::vector<uint8_t> data(10, 20);
+    // node.put("other_unique_key", data);
+
+    // // put some data on the dht, signed with our generated private key
+    // node.putSigned("other_unique_key_42", data);
+
+    // std::vector<uint_8> MK
+
+    // get data from the dht
+    // node.get("K7", [](const std::vector<std::shared_ptr<dht::Value>>& values) {
+    //     // Callback called when values are found
+    //     for (const auto& value : values)
+    //     {
+    //         std::cout << "Found value: " << *value << std::endl;
+    //         // std::cout << dht::crypto::PublicKey({'K', '5'}).encrypt({5,10}) << std::endl;
+    //         // std::cout << "Decrypted: " << decrypt(*value) << std::endl;
+    //     }
+    //     return true; // return false to stop the search
+    // });
+
+
+    std::cout << "P Data" << std::endl;
+    // sleep(120);
+    // int i = 0;
+    // while(true)
+    // {
+    //     ++i;
+    // }
+
+    std::cout << "-----------------" << std::endl;
+
+
+    // wait for dht threads to end
+
+
+
+
+
+
+
+
 	returnStatus = fuse_main(argc, argv, &operations, NULL);
 
 	closedir(mountpoint.dir->dp);
 	free(mountpoint.path);
+
+
+
+	    node.join();
+
 
 	return returnStatus;
 }
