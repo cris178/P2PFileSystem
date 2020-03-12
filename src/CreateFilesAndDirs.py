1d31e7f8421279e8e8cae2fe7e7cc1c394bd161b
danny <danialesaid@gmail.com>
2020-03-11 20:06:08 -0700

    create script

---
+++ src/CreateFilesAndDirs.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+os.chdir("Mountingpoint")
+
+for i in range(10):
+    with open('F' + str(i) + '.txt', 'w') as FW:
+        time.sleep(1)
+        FW.write( "This is File:" + str(i))
+        time.sleep(1)
+
+for i in range(5):
+    newDirectory = 'D' + str(i)
+    os.mkdir(newDirectory)
+    time.sleep(1)
+    os.chdir(newDirectory)
+    time.sleep(1)
+    with open('FD' + str(i) + '.txt', 'w') as FW:
+        FW.write( "This is File in D:" + str(i))
+        time.sleep(1)
